5e7b5174fa1dc3c883f1027a3cbd91bfda680971
oshanedesign <oshane.design@gmail.com>
2022-04-08 17:31:37 -0500

    git bisect test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import './style.css';
-// import * as API from './modules/LeaderboardAPI.js';
+import * as API from './modules/LeaderboardAPI.js';
+
+const refresh = document.querySelector('.recent-scores-refresh-button');
+const form = document.querySelector('.add-your-scores-container');
+
+document.addEventListener('DOMContentLoaded', API.showScores);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input1 = document.querySelector('#name').value;
+    const input2 = document.querySelector('#score').value;
+
+    API.newAddScore(input1, input2);
+    form.requestFullscreen();
+
+})
